a8a593bb488a6911d8b46e3ad01fc06855935d9f
KANHA <www.pcplayer1@gmail.com>
2026-09-04 14:14:24 +0530

    hello new

---
+++ hello.py
@@ -0,0 +1,5 @@
+# This is a simple program that asks the user for their name and then greets them in several different ways.
+# ASK USER FOR THEIR NAME
+name = input("what's your name? ").strip().title()
+first, last = name.split(" ")
+print (f"hello, {last}")
